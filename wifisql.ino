2ae23da61fa50824309c62aa2022d5b3607cc040
blackerking <2019363+blackerking@users.noreply.github.com>
2021-03-23 12:19:15 +0100

    FINAL Version
    All bugs seems eliminated.
    * Tries to reconnect if WLAN dropped
    * Tries to reconnect if SQL-Connection dropped
    * Restarts if no connection is established after 1 minute
    * No strings -> means no RAM fragmentation (what a waste of libraries)
    * LED light means everything works - no light means Error
    * Easy to use thanks to define rules at the top
    * complete in english
    * No handling of missing sensor!

--- wifisql.ino
+++ wifisql.ino
@@ -2,7 +2,8 @@
     This Application for an ESP8266 WiFi MicroController uses a DHT22 Sensor to get temperatur and humidity.
     After this it stores this information inside a mysql database via WiFi connection.
 
-    This code is a kind of merge of different sketches.
+    This code is a kind of merge of different sketches and tutorials.
+    Testrun was more than 48h successful. 
 */
 
 #include <ESP8266WiFi.h>
@@ -10,24 +11,35 @@
 #include <MySQL_Cursor.h>
 #include "DHT.h"
 
-#define DHTPIN      D2              // Digital pin connected to the DHT sensor 
-#define DHTTYPE     DHT22           // DHT 22 
-#define STASSID     "SSID"          // SSID of your WLAN
-#define STAPSK      "PASSWORD"      // Password of your WLAN
-#define SQLUSER     "ROOT"
-#define SQLPASS     "SQLPASS"
-#define BAUDRATE    9600
-#define ROOMNR      2
+//--------------NEEDED WLAN INFORMATION-------------------------//
+#define DHTPIN      D2                  // Digital pin connected to the DHT sensor 
+#define DHTTYPE     DHT22               // DHT 22 (AM2302)
+#define STASSID     "SSID"              // SSID of your WLAN
+#define STAPSK      "LONGPASSWORD"      // Password of your WLAN
+//--------------NEEDED DB INFORMATION--------------------------//
+#define SERVER      192,168,1,2         // IP-Address of your Server - coma instead of points
+#define SQLUSER     "SQLUSER"           // User of your database-server
+#define SQLPASS     "dkjfhgkjdf"        // Password of your database-server
+#define DBNAME      "USE roomtemp;"     // databasename
+#define ROOMNR      "3"
 
-const char* ssid         = STASSID;
+//------------------------------------------------------------//
+#define VERSION     "1.0 - 2021.03.23"
+
+void(* resetFunc) (void) = 0;
+const char* ssid     = STASSID;
 const char* wlanpassword = STAPSK;
 DHT dht(DHTPIN, DHTTYPE);
 
-IPAddress server_addr(0,0,0,0);     // IP of the MySQL *server* here ; uses comata as points!
-char user[]         = SQLUSER;      // MySQL user login username
-char password[]     = SQLPASS;      // MySQL user login password
-char USE_DB_SQL[]   = "USE DATABASE;";
+IPAddress server_addr(SERVER);       // IP of the MySQL *server* here
+char user[] = SQLUSER;                   // MySQL user login username
+char password[] = SQLPASS;               // MySQL user login password
+const char USE_DB_SQL[] = DBNAME;
 uint32_t delayMS;
+int lostconnection = 0;
+char resultt[8];
+char resulth[8];
+
 float h;
 float t;
 float hold;
@@ -38,36 +50,13 @@ WiFiClient client;
 MySQL_Connection conn(&client);
 MySQL_Cursor* cursor;
 
-void setup() {
-  Serial.begin(9600);
-  dht.begin();
-  // We start by connecting to a WiFi network
-  delay(500); // for serialmonitor
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, wlanpassword);
-  int count = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    count++;
-      if(count == 20)
-      {
-        h = dht.readHumidity();
-        t = dht.readTemperature();
-        Serial.println("");
-        Serial.println("No WiFi connection established! Try again...");
-        Serial.print(F("Temperature: "));
-        Serial.print(t);
-        Serial.print(F("°C - Humidity: "));
-        Serial.print(h);
-        Serial.println();
-        count =0;
-      }
-    }
+// Use WiFiClient class to create TCP connections
+WiFiClient client;
+MySQL_Connection conn(&client);
+MySQL_Cursor* cursor;
+
+void connectsql(void)
+{
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -77,15 +66,54 @@ void setup() {
   if (conn.connect(server_addr, 3306, user, password))
     Serial.println("OK.");
   else
+  {
     Serial.println("FAILED.");
-  
+    return;
+  }
   // create MySQL cursor object
   cursor = new MySQL_Cursor(&conn);
 }
 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  dht.begin();
+  // We start by connecting to a WiFi network
+  delay(500); // for serialmonitor
+  Serial.println();
+  Serial.println();
+  Serial.println("Simple ESP-WIFI-TEMP to MYSQL Server Chip");
+  Serial.println("Version: ");
+  Serial.println(VERSION);
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, wlanpassword);
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    Serial.print(".");
+    count++;
+    digitalWrite(LED_BUILTIN, HIGH);
+      if(count == 20)
+      {
+        h = dht.readHumidity();
+        t = dht.readTemperature();
+        Serial.println("");
+        Serial.println("No WiFi connection established! Try again...");
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.print(F("Temperature: "));
+        Serial.print(t);
+        Serial.print(F("°C - Humidity: "));
+        Serial.print(h);
+        Serial.println();
+        count =0;
+      }
+    }
+ connectsql();
+}
+
 void loop() {
-
-
   delay(5000);
   h = dht.readHumidity();
   // Read temperature as Celsius (the default)
@@ -99,37 +127,57 @@ void loop() {
   Serial.println(); 
 
   //Check WIFI
-if(WiFi.status() != WL_CONNECTED)
+  if(WiFi.status() != WL_CONNECTED)
   {
+    if(lostconnection >= 30)
+    {
+       Serial.println("No Wifi connection about more than a Minute, Try restart...");
+       resetFunc();
+    }
     Serial.println("WiFi connection lost, Try reconnnecting...");
     WiFi.begin(ssid, wlanpassword);
+    lostconnection++;
+    digitalWrite(LED_BUILTIN, HIGH);
     return;
    }
+  if((WiFi.status() == WL_CONNECTED) && (lostconnection >= 1))
+  {
+    lostconnection = 0;
+    connectsql();
+  }
 
-if((h == hold) && (told == t))
-{
-  //Nothing changed
-  Serial.println("No new Values...");
-  return;
-}
-  char sqlcmdc[99] ="INSERT INTO wohnzimmer (temp, humidity, roomnr) VALUES (";
-  char resultt[8];
-  char resulth[8];
-  dtostrf(t,2,2,resultt);
-  dtostrf(h,2,2,resulth);
-
-  strcat(sqlcmdc, resultt);
-
-  strcat(sqlcmdc, ",");
-  strcat(sqlcmdc, resulth);
-  strcat(sqlcmdc, ",ROOMNR)\0");
-  Serial.println(sqlcmdc);
-
+  if((h == hold) && (told == t))
+  {
+    //Nothing changed
+    Serial.println("No new Values...");
+    digitalWrite(LED_BUILTIN, LOW);
+    return;
+  }
+  if(((h != hold) || (told != t))  && (WiFi.status() == WL_CONNECTED))
+  {
+    char sqlcmdc[99] ="INSERT INTO tblmessungen (temp, humidity, roomnr) VALUES (";
+    dtostrf(t,2,2,resultt);
+    dtostrf(h,2,2,resulth);
+    strcat(sqlcmdc, resultt);
+    strcat(sqlcmdc, ",");
+    strcat(sqlcmdc, resulth);
+    strcat(sqlcmdc, ",");
+    strcat(sqlcmdc, ROOMNR);
+    strcat(sqlcmdc, ")\0");
+    Serial.println(sqlcmdc);
   if (conn.connected())
+  {
     cursor->execute(USE_DB_SQL);
     cursor->execute(sqlcmdc);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    Serial.println("No Connection to SQL Server!\n");
+  }
   hold = h;
   told = t;
 
- // delay(300000); // execute once every 5 minutes, don't flood remote service
+  // delay(300000); // execute once every 5 minutes, don't flood remote service
+  }
 }
